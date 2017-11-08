2b28f6e0c5e64cfc147b9a5a6c3af98e2869baed
James McMillan <jmcmillan29@gmail.com>
2017-11-07 20:44:43 -0800

    fixed a bug with the topic buttons so that they are all now individual buttons

--- assets/javascript/view.js
+++ assets/javascript/view.js
@@ -1,13 +1,13 @@
  // This is where most of the JS & jQuery will live
  // for the project that will manipulate our webpage
 
-window.onload = () => {
+$(document).ready(function(){
 
 	view.displayButtons();
 	controller.topicButtonClick();
 	controller.playGifs();
 
-};
+});
 
 // view Object
 
@@ -15,14 +15,11 @@ var view = {
 
 	displayButtons: () => {
 
-		var topicButtons = $('<button>')
-
 		for (var i = 0; i < topics.length; i++) {
-			var topic = $('<li>').addClass('topic-button').text(topics[i]);
+			var topicButtons = $('<button>').addClass('topic-button btn').text(topics[i]);
 				for (var j = 0; j < buttonClasses.length; j++) {
-				topic.addClass('btn '+buttonClasses[j])
+				topicButtons.addClass(buttonClasses[j])
 			}
-			topicButtons.append(topic);
 			$('.button-display').append(topicButtons);
 		}
 
